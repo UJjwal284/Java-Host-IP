e88dc90eb1fddba05eb0fdd309ce7c5ec11e5b67
UJjwal284 <ujjwalpr28@protonmail.com>
2023-11-07 17:10:17 +0530

    Revert "Initial"
    This reverts commit 3049681220d7438422ed630f5442dfb9619e4831.

--- src/main/java/com/example/tfappapi/DetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tfappapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetailsRepository extends JpaRepository<DetailsEntity, Long> {
-    DetailsEntity findByName(String admin);
-}
